f774f854b3c435b3d20025e3b4894e45da5326fb
OrPoplinger10 <orpoplinger777@gmail.com>
2023-04-16 17:20:30 +0300

    Added Butoon remove hover

--- project.js
+++ project.js
@@ -80,7 +80,7 @@ function displayBoardOnScreen(taskList) {
 function createMissionBoard(data) {
   const newBoard = document.createElement("div");
    newBoard.classList.add("col-5", "mission-card");
-   newBoard.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 24 24" fill="none" stroke="#000000" 
+   newBoard.innerHTML = `<svg class= "button-remove" xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 24 24" fill="none" stroke="#000000" 
      stroke-width="3" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18">
     </line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
     <p class = "date">${data.formDate}</p>
